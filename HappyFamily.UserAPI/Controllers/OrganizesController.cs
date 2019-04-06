e314385c12f04024328b0d868ad49eb6e3e3ec69
neolu <1113865828@qq.com>
2019-04-06 23:40:15 +0800

    feat:新增机构管理

---
+++ HappyFamily.UserAPI/Controllers/OrganizesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HappyFamily.UserAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrganizesController : ControllerBase
+    {
+
+        private readonly Sys_OrganizeManager sys_OrganizeManager = new Sys_OrganizeManager();
+        // GET: api/Organizes
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Organizes/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Organizes
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Organizes/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
